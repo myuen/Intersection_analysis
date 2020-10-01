7645a897ae6c2c19793ac09ca51b9b49f5fde5d1
mack <myuen@mail.ubc.ca>
2020-10-01 10:36:13 -0700

    Added comments.  Change label size and made other cosmetic changes

--- analysis/helper04_make-volcano-plot.R
+++ analysis/helper04_make-volcano-plot.R
@@ -12,28 +12,38 @@ makeVolcanoPlot <- function(df, pCutoff, lfcCutoff, pTitle) {
         y = -log10(adj.P.Val),
         color = focus_term,
         shape = focus_term)) + 
-    geom_point(size = 0.9) + 
+    geom_point(size = 2.5) + 
     
-    scale_colour_manual(values = c("black", "grey")) +
+    scale_colour_manual(values = c("black", "gray50")) +
     
+    # Add break points on x-axis to show min, max, log2FC threshold
     scale_x_continuous(breaks = c(minX, -2, 0, 2, maxX)) +
     
+    # Add break point to show adj p-value threshold
+    # -log(0.05) = 1.3
     scale_y_continuous(breaks = c(0, 1.3), labels = c(0, 0.05)) +
     
     theme_classic() +
-    theme(panel.grid.major = element_blank(), 
+    
+    # Define text size and remove grid lines
+    theme(text = element_text(size = 16),
+          legend.text = element_text(size = 16),
+          panel.grid.major = element_blank(), 
           panel.grid.minor = element_blank()) +
     
+    # Add line to show log2FC and adj p-value threshold
     geom_abline(aes(intercept = -log10(pCutoff), slope = 0), colour = "blue") + 
     geom_vline(xintercept = lfcCutoff, colour = "blue") + 
     geom_vline(xintercept = -(lfcCutoff), colour = "blue") +
     
+    # Add title for plot, x and y-axis
     labs(title = pTitle,
-         x = expression(paste('Log'[2], " Fold Change")), 
-         y = expression(paste('-Log'[10], " Adj. p-Value"))) +
+         x = expression(paste('Log'[2], 'Fold Change')), 
+         y = expression(paste('-Log'[10], 'Adj. p-Value'))) +
     
-    guides(colour = guide_legend("", keywidth = .5),
-           shape = guide_legend("", keywidth = .5))
+    # Define legend
+    guides(colour = guide_legend("", keywidth = 1.5),
+           shape = guide_legend("", keywidth = 2.5))
   
   return (g)
 }

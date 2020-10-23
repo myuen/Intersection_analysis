6889189502ea6a3f6482ef8226b88d1ed0d6b440
mack <myuen@mail.ubc.ca>
2020-10-23 16:54:45 -0700

    Added comment.  Wrap lines for better readability.  Break out subsetting into a separate file.

--- analysis/16-1_process-BLAST-annotations.R
+++ analysis/16-1_process-BLAST-annotations.R
@@ -3,7 +3,7 @@ library(purrr)
 library(stringr)
 library(tidyr)
 
-source("analysis/helper01_getLineage.R")
+# Process BLAST results. Keeping only tophit and extract IDs from description.
 
 
 # BLAST results output on tab-delimited format (i.e. run with -outfmt '6' option
@@ -15,26 +15,37 @@ blast <-
              colClasses = c(rep("character", 2), 
                             rep("numeric", 14), rep("character", 5)))
 
-blast <- blast %>% select(qseqid, sseqid, evalue, salltitles)
+blast <- blast %>% 
+  select(qseqid, sseqid, evalue, salltitles)
 
 str(blast)
 # 'data.frame':	50830 obs. of  4 variables:
 
 
 # Extract only the top hit for downstream analysis
-topBlastHit <- blast %>% group_by(qseqid) %>% nest()
+topBlastHit <- blast %>% 
+  group_by(qseqid) %>% 
+  nest()
 
 topBlastHit$data <- map(topBlastHit$data, function(x) {
   # Extract UniProt ID from sseqid
-  x[1, "sseqid"] <- str_split(x[1 ,"sseqid"], "\\|") %>% unlist() %>% nth(2)
+  x[1, "sseqid"] <- 
+    str_split(x[1 ,"sseqid"], "\\|") %>% 
+    unlist() %>% 
+    nth(2)
   
   # Extract tax id from description
-  x[1, "tax_id"] <- str_split(x[1 ,"salltitles"], "=") %>% unlist %>% nth(3) %>% 
-    str_replace(" GN", "") %>% str_replace(" PE", "")
+  x[1, "tax_id"] <- str_split(x[1 ,"salltitles"], "=") %>% 
+    unlist %>% 
+    nth(3) %>% 
+    str_replace(" GN", "") %>% 
+    str_replace(" PE", "")
+
   x[1,]
 })
 
-topBlastHit <- topBlastHit %>% unnest(c(data))
+topBlastHit <- topBlastHit %>% 
+  unnest(c(data))
 
 topBlastHit$tax_id <- as.integer(topBlastHit$tax_id)
 
@@ -45,41 +56,5 @@ colnames(topBlastHit) <-
 str(topBlastHit)
 # tibble [9,714 × 5] (S3: grouped_df/tbl_df/tbl/data.frame)
 
-
-# Read cd_vs_wiq set analysis results
-cd_vs_wiq <- read.table("results/cd_vs_wiq.txt", 
-                        header = TRUE, stringsAsFactors = FALSE)
-
-str(cd_vs_wiq)
-# 'data.frame':	9298 obs. of  6 variables:
-
-
-# We have BLASTed more sequences than needed.  Only keeping those we need for
-# downstream analysis
-topBlastHit <- left_join(cd_vs_wiq, topBlastHit)
-# Joining, by = "cds"
-
-
-# Let's extract all unique taxonomy ID from all top blast hit
-uniq.TaxIDs <- unique(sort(topBlastHit$tax_id))
-length(uniq.TaxIDs)
-# [1] 381
-
-uniq.TaxIDs <- getLineage(uniq.TaxIDs) %>% 
-  select(old_tax_id, Taxonomy)
-
-colnames(uniq.TaxIDs)[1] <- "tax_id"
-
-str(uniq.TaxIDs)
-# 'data.frame':	381 obs. of  2 variables:
-
- 
-topBlastHit <- left_join(topBlastHit, uniq.TaxIDs) 
-# Joining, by = "tax_id"
-
-str(topBlastHit)
-# 'data.frame':	9298 obs. of  11 variables:
-
-# Table S2
 write.table(topBlastHit, "results/all-DE.blastpUniProt.topHit.txt",
             sep = "\t", quote = FALSE, row.names = FALSE)

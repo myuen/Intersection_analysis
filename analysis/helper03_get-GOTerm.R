7aa11f13f84f0d5cab8f39ae69c28667a818bdfa
mack <myuen@mail.ubc.ca>
2020-10-23 15:44:57 -0700

    Line wrapping

--- analysis/helper03_get-GOTerm.R
+++ analysis/helper03_get-GOTerm.R
@@ -8,6 +8,7 @@ getGOTerm <- function(x){
   uId_2_goTerm <- subset(go.db, go.db$GOID %in% x)
   
   # Only keep Biological Process terms
-  uId_2_goTerm <- uId_2_goTerm %>% filter(ONTOLOGY == "BP") %>%
+  uId_2_goTerm <- uId_2_goTerm %>%
+    filter(ONTOLOGY == "BP") %>%
     dplyr::select(-ONTOLOGY)
 }
